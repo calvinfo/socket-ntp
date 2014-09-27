0f18939775daad834931fb7e401b46be7e63939d
karaokeresearch <krc@brassrocket.com>
2014-09-26 21:03:15 -0700

    fake-ntp, especially useful for smartphones or other devices that have >3 or 4 ms lag even on networks with <5ms lag, probably due to a less-than-robust wireless card, such as my Samsung Galaxy s2.  This code takes the best 4 pings out of 20 and uses the data from those since lower pings = less margin for error.  Still has tons of debug code in there, for now.

--- client/ntp.js
+++ client/ntp.js
@@ -14,21 +14,35 @@
 
   var onSync = function (data) {
 
-    var diff = Date.now() - data.t1 + ((Date.now() - data.t0)/2);
+    var diff = Date.now() - data.t1 - ((Date.now() - data.t0)/2);
+    var ping = Date.now() - data.t0;
+    offsets.unshift([ping,diff]);
 
-    offsets.unshift(diff);
-
-    if (offsets.length > 10)
+    if (offsets.length > 20){
       offsets.pop();
+    }
+      
   };
 
 
   ntp.offset = function () {
     var sum = 0;
-    for (var i = 0; i < offsets.length; i++)
-      sum += offsets[i];
+    var offsetsSorted = offsets.slice(0); //make a copy of the offsets array for sorting.  Keep the original so the times keep cycling.
+    var whee ="";
+    offsetsSorted.sort(sort2dArray);
 
-    sum /= offsets.length;
+    if (offsetsSorted.length >4){ //get rid of everything except the 4 lowest ping times.
+		    for (var i = 0; i < offsets.length-3; i++) {
+		    offsetsSorted.pop();
+		    }
+    }
+    for (var i = 0; i < offsetsSorted.length; i++) {
+      sum += offsetsSorted[i][1];
+		  whee = whee + " " + offsetsSorted[i][0];
+	
+		}
+    sum /= offsetsSorted.length;
+    console.log(whee);
 
     return sum;
   };
@@ -37,6 +51,16 @@
   var sync = function () {
     socket.emit('ntp:client_sync', { t0 : Date.now() });
   };
+ 
+	 var sort2dArray = function (a, b) {
+	    if (a[0] === b[0]) {
+	        return 0;
+	    }
+	    else {
+	        return (a[0] < b[0]) ? -1 : 1;
+	    }
+	} 
+  
 
   // AMD/requirejs
   if (typeof define === 'function' && define.amd) {
